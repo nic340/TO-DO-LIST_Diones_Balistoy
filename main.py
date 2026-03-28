afe63a74640b935edc0f17fef8c3e6b35205a7dd
nic340 <nicolediones5@gmail.com>
2026-03-28 11:58:10 -0800

    Fix TemplateResponse context for Python 3.12 compatibility

--- main.py
+++ main.py
@@ -13,7 +13,8 @@ tasks = []
 
 @app.get("/")
 def read_tasks(request: Request):
-    return templates.TemplateResponse("index.html", {"request": request, "tasks": tasks})
+    context = dict(request=request, tasks=tasks)
+    return templates.TemplateResponse("index.html", context)
 
 @app.post("/add")
 def add_task(request: Request, task: str = Form(...)):
