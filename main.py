ad9cf69522fc39eee231142e24dfc5a96c514e89
nic340 <nicolediones5@gmail.com>
2026-03-29 02:44:20 -0800

    changes

--- main.py
+++ main.py
@@ -6,22 +6,21 @@ from fastapi.templating import Jinja2Templates
 app = FastAPI()
 
 app.mount("/static", StaticFiles(directory="static"), name="static")
-
 templates = Jinja2Templates(directory="templates")
 
 tasks = []
 
 @app.get("/")
-def read_tasks(request: Request):
-    return templates.TemplateResponse("index.html", {"request": request, "tasks": tasks})
+async def read_tasks(request: Request):
+    return templates.TemplateResponse(name="index.html", context={"request": request, "tasks": tasks})
 
 @app.post("/add")
-def add_task(request: Request, task: str = Form(...)):
+async def add_task(request: Request, task: str = Form(...)):
     tasks.append(task)
     return RedirectResponse(url="/", status_code=303)
 
 @app.get("/delete/{task_id}")
-def delete_task(task_id: int):
+async def delete_task(task_id: int):
     if 0 <= task_id < len(tasks):
         tasks.pop(task_id)
     return RedirectResponse(url="/", status_code=303)
